6fae0a37d6efd3f56e216f77703e7b41cd9a6b90
766F6964 <766F6964@protonmail.com>
2023-06-27 00:24:09 +0200

    First attempts to get filtering by tags working. Currently not fully functional

--- src/js/tags.js
+++ src/js/tags.js
@@ -1,19 +1,52 @@
-var tag_mapping = {};
+var tag_mapping = {
+    "posts/inland_taipan.html" : ["venomous snakes","inland taipan","reptiles","australian wildlife","venomous animals"],
+    "posts/giant_pacific_octopus.html" : ["marine predators","giant pacific octopus","aquatic wildlife"],
+    "posts/great_white_shark.html" : ["marine predators" ,"great white shark", "aquatic wildlife","shark facts"],
+    "posts/komodo_dragon.htm" : ["reptiles","monitor lizards","komodo dragon","wildlife conservation","venomous animals"],
+    "posts/black_mamba.html" : ["venomous snakes","black mamba","reptiles","african wildlife","venomous animals"]
+};
 
-function select_tag(tag) {
-    console.log("------------------------");
+function get_tag_parameter() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const tag = urlParams.get('tag');
+    if (tag) {
+        console.log(`Tag: ${tag}`);
+        filter_by_tag(tag);
+    } else {
+        console.log('Tag parameter not found in the URL');
+    }
+}
 
-    matching_posts= [];
+function hide_article_listing(post_url) {
+    article_list = document.getElementsByClassName("articles")[0];
+    list_entries = article_list.getElementsByTagName('li');
 
-    console.log("Tag %s matches these posts:", tag);
-    for (var path in tag_mapping) {
-        if (tag_mapping[path].includes(tag)) {
-            console.log(path);
+    for (var i = 0; i < list_entries.length; i++) {
+        path = list_entries[i].children[1].pathname;
+        console.log("path: " + path);
+        console.log("post_url: " + post_url);
+        if (path.includes(post_url) == false) {
+            console.log("Hiding: " + post_url);
+            list_entries[i].style.display = 'none';
+            break;
         }
     }
-
-    // Remove tag entries
-    //let tag_elems = document.getElementsByClassName("tags")[0];
-    //tag_elems.remove();
-    //console.log("Cleared tag elements");
 }
+
+function filter_by_tag(tag) {
+    for (var item in tag_mapping) {
+      if (tag_mapping.hasOwnProperty(item)) {
+        var tags = tag_mapping[item];
+        for (var i = 0; i < tags.length; i++) {
+          var current_tag = tags[i];
+          if (current_tag == tag) {
+            hide_article_listing(item);
+            break;
+          }
+        }
+      }
+    }
+    console.log(article_list);
+}
+
+document.addEventListener("DOMContentLoaded", get_tag_parameter);
