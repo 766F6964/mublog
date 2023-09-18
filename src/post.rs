82df05091a739f39186cff7d0d07e5996a4c9d71
766F6964 <766F6964@protonmail.com>
2023-09-19 01:12:14 +0200

    Add unit tests for parsing basic post header structure

--- src/post.rs
+++ src/post.rs
@@ -1,4 +1,4 @@
-use anyhow::{anyhow, Context, Ok};
+use anyhow::{anyhow, Context, Ok, Result};
 use std::fs;
 use std::io::prelude::*;
 use std::io::{self, BufReader};
@@ -13,12 +13,7 @@ struct Post {
     title: String,
 }
 
-pub fn from_file(filepath: &Path) -> anyhow::Result<()> {
-    println!("Parsing {}", filepath.display());
-    let file = fs::File::open(filepath).context("Failed to open file.")?;
-    let reader = BufReader::new(file);
-    let lines: Vec<String> = reader.lines().map(|line| line.unwrap()).collect();
-
+fn parse_header(lines: Vec<String>) -> Result<()> {
     // Ensure minimal length required for header
     if lines.len() < 7 {
         return Err(anyhow!("File contains an incomplete header."));
@@ -60,58 +55,105 @@ pub fn from_file(filepath: &Path) -> anyhow::Result<()> {
                 println!("Draft value: {}", value)
             }
             _ => {
-                return Err(anyhow!("Unsupported header field"))?;
+                return Err(anyhow!("Unsupported header field: {}", key))?;
             }
         }
     }
+    Ok(())
+}
+pub fn from_file(filepath: &Path) -> anyhow::Result<()> {
+    println!("Parsing {}", filepath.display());
+    let file = fs::File::open(filepath).context("Failed to open file.")?;
+    let reader = BufReader::new(file);
+    let lines: Vec<String> = reader.lines().map(|line| line.unwrap()).collect();
+    _ = parse_header(lines);
     // TODO:
     // - Check that file extension is .md/.MD
     // - Check that end marker is present
     // - After parsing, verify all fields are set.
     // - Add dedicated methods to parse each field
     // - Ensure proper error propagation on failure
-
-    // if filepath.exists() && filepath.is_file() {
-    //     let file = fs::read_to_string(filepath)
-    //         .with_context(|| format!("Failed to read file {}", filepath.display()))?;
-    //     let mut lines = file.lines();
-
-    //     // Ensure first line is the starting marker.
-    //     let start_marker = lines.next().unwrap_or_default();
-    //     if start_marker != "---" {
-    //         return Err(anyhow::anyhow!("No starting marker found in post header."))?;
-    //     }
-    //     // Ensure the presence of all header fields.
-    //     for line in lines {
-    //         let (key, value) = line
-    //             .split_once(":")
-    //             .with_context(|| format!("Failed to parse line '{}' into key value pair.", line))?;
-    //         match key {
-    //             "title" => {
-    //                 println!("Title value: {}", value)
-    //             }
-    //             "description" => {
-    //                 println!("Description value: {}", value)
-    //             }
-    //             "date" => {
-    //                 println!("Date value: {}", value)
-    //             }
-    //             "tags" => {
-    //                 println!("Tags value: {}", value)
-    //             }
-    //             "draft" => {
-    //                 println!("Draft value: {}", value)
-    //             }
-    //             _ => {
-    //                 return Err(anyhow::anyhow!("Unsupported header field"));
-    //             }
-    //         }
-    //     }
-
-    //     let end_marker = lines.next().unwrap_or_default();
-    //     if end_marker != "---" {
-    //         return Err(anyhow::anyhow!("No ending marker found in post header."))?;
-    //     }
-    // }
     Ok(())
+}
+
+#[cfg(test)]
+mod post_test {
+    use super::{from_file, parse_header};
+
+    #[test]
+    fn parse_header_incomplete() {
+        let expected = "---
+title: test title
+description: test description
+---"
+        .lines()
+        .map(String::from)
+        .collect();
+
+        let res = parse_header(expected).unwrap_err();
+        assert_eq!(res.to_string(), "File contains an incomplete header.");
+    }
+
+    #[test]
+    fn parse_header_missing_start_marker() {
+        let expected = "title: test title
+description: test description
+tags: test,test2,test3
+date: 2023-01-23
+draft: false
+---
+more data
+some more text"
+            .lines()
+            .map(String::from)
+            .collect();
+
+        let res = parse_header(expected).unwrap_err();
+        assert_eq!(
+            res.to_string(),
+            "Starting marker missing or formatted incorrectly"
+        );
+    }
+
+    #[test]
+    fn parse_header_missing_end_marker() {
+        let expected = "---
+title: test title
+description: test description
+tags: test,test2,test3
+date: 2023-01-23
+draft: false
+
+some more data
+goes here
+"
+        .lines()
+        .map(String::from)
+        .collect();
+
+        let res = parse_header(expected).unwrap_err();
+        assert_eq!(
+            res.to_string(),
+            "Ending marker missing or formatted incorrectly"
+        );
+    }
+
+    #[test]
+    fn parse_header_unsupported_field() {
+        let expected = "---
+title: test title
+description: test description
+unsupported: test,test2,test3
+date: 2023-01-23
+draft: false
+---
+some more data
+"
+        .lines()
+        .map(String::from)
+        .collect();
+
+        let res = parse_header(expected).unwrap_err();
+        assert_eq!(res.to_string(), "Unsupported header field: unsupported");
+    }
 }
