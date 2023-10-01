80f319540541daa173aa3c1dbe6f8bfcd186ca06
766F6964 <766F6964@protonmail.com>
2023-10-01 20:47:05 +0200

    Initial work on wrapping posts in HTML layout

--- src/stages/wrap_posts_stage.rs
+++ src/stages/wrap_posts_stage.rs
@@ -8,8 +8,31 @@ impl PipelineStage for WrapPostsStage {
         Ok(())
     }
 
-    fn process(&self, _ctx: &mut BlogContext) -> anyhow::Result<()> {
+    fn process(&self, ctx: &mut BlogContext) -> anyhow::Result<()> {
         println!("WrapPostsStage: Process ...");
+        for post in &mut ctx.posts {
+            let mut post_html = String::new();
+            post_html.push_str(html_tag_start().as_str());
+            post_html.push_str(head_tag_start().as_str());
+            post_html.push_str(&meta_tags(
+                &post.header.title,
+                "TODO_AUTHOR",
+                "TODO_URL",
+                "TODO_DESCRIPTION",
+                "TODO_DESCRIPTION",
+                &post.header.title,
+                "TODO_URL",
+                post.header.date.to_string().as_str(),
+            ));
+            post_html.push_str(stylesheet_refs().as_str());
+            post_html.push_str(head_tag_end().as_str());
+            post_html.push_str(body_tag_start().as_str());
+            post_html.push_str(&post.content);
+            post_html.push_str(body_tag_end().as_str());
+            post_html.push_str(html_tag_end().as_str());
+
+            post.content = post_html;
+        }
         Ok(())
     }
 
@@ -17,4 +40,77 @@ impl PipelineStage for WrapPostsStage {
         println!("WrapPostsStage: Finalize ...");
         Ok(())
     }
+}
+
+fn html_tag_start() -> String {
+    return r#"
+<!DOCTYPE html>
+<html lang="en-US">"#
+        .into();
+}
+
+fn html_tag_end() -> String {
+    return r#"
+    </html>"#
+        .into();
+}
+
+fn head_tag_start() -> String {
+    return r#"
+    <head>"#
+        .into();
+}
+fn head_tag_end() -> String {
+    return r#"
+    </head>"#
+        .into();
+}
+
+fn meta_tags(
+    blog_title: &str,
+    blog_author: &str,
+    blog_url: &str,
+    blog_description: &str,
+    post_description: &str,
+    post_title: &str,
+    post_url: &str,
+    post_date: &str,
+) -> String {
+    return format!(
+        r#"
+        <meta charset="utf-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="robots" content="index, archive">
+        <meta name="canonical" content="{blog_url}">
+        <meta name="description" content="{blog_description}">
+        <meta property="og:type" content="article" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:site_name" content="${blog_title}" />
+        <meta property="og:title" content="{post_title}" />
+        <meta property="og:description" content="${post_description}" />
+        <meta property="og:url" content="{post_url}/${post_title}.html" />
+        <meta property="og:article:published_time" content="{post_date}" />
+        <meta property="og:article:author" content="{blog_author}" />"#
+    )
+    .into();
+}
+
+fn stylesheet_refs() -> String {
+    return r#"
+    <link rel="stylesheet" type="text/css" media="all" href="/css/layout.css">"#
+        .into();
+}
+fn body_tag_start() -> String {
+    return r#"
+        <body class="${blog_theme}">
+        <main>
+        <hr>"#
+        .into();
+}
+fn body_tag_end() -> String {
+    return r#"
+        </main>
+        </body>"#
+        .into();
 }
