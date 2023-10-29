571a7a3bbfa04f677e4313560712ba39188d6604
766F6964 <766F6964@protonmail.com>
2023-10-29 21:07:09 +0100

    Test: Add pipeline feature tests
    Added unit test that ensures that pipeline features can modify stages.
    Added unit test that ensures that pipeline features, that modify the
    same stages, are executed in the order, they got added to the pipeline.

--- src/pipeline/mod.rs
+++ src/pipeline/mod.rs
@@ -64,67 +64,133 @@ impl Pipeline {
 
 #[cfg(test)]
 mod test {
+    use super::feature::Feature;
+    use super::pipeline_stage_lifetime::PipelineStageLifetime;
     use crate::blog::BlogContext;
     use crate::pipeline::pipeline_stage::PipelineStage;
     use crate::pipeline::Pipeline;
+    use std::any::TypeId;
+    struct DummyStage1;
+    struct DummyStage2;
+    struct DummyStage3;
+    struct DummyFeature1;
+    struct DummyFeature2;
+
+    impl PipelineStage for DummyStage1 {
+        fn initialize(&self, ctx: &mut BlogContext) -> anyhow::Result<()> {
+            ctx.config.blog_author.push_str("1i");
+            Ok(())
+        }
+
+        fn process(&self, ctx: &mut BlogContext) -> anyhow::Result<()> {
+            ctx.config.blog_author.push_str("1p");
+            Ok(())
+        }
+
+        fn finalize(&self, ctx: &mut BlogContext) -> anyhow::Result<()> {
+            ctx.config.blog_author.push_str("1f");
+            Ok(())
+        }
+    }
+
+    impl PipelineStage for DummyStage2 {
+        fn initialize(&self, ctx: &mut BlogContext) -> anyhow::Result<()> {
+            ctx.config.blog_author.push_str("2i");
+            Ok(())
+        }
+
+        fn process(&self, ctx: &mut BlogContext) -> anyhow::Result<()> {
+            ctx.config.blog_author.push_str("2p");
+            Ok(())
+        }
+
+        fn finalize(&self, ctx: &mut BlogContext) -> anyhow::Result<()> {
+            ctx.config.blog_author.push_str("2f");
+            Ok(())
+        }
+    }
+
+    impl PipelineStage for DummyStage3 {
+        fn initialize(&self, ctx: &mut BlogContext) -> anyhow::Result<()> {
+            ctx.config.blog_author.push_str("3i");
+            Ok(())
+        }
+
+        fn process(&self, ctx: &mut BlogContext) -> anyhow::Result<()> {
+            ctx.config.blog_author.push_str("3p");
+            Ok(())
+        }
+
+        fn finalize(&self, ctx: &mut BlogContext) -> anyhow::Result<()> {
+            ctx.config.blog_author.push_str("3f");
+            Ok(())
+        }
+    }
+
+    impl Feature for DummyFeature1 {
+        fn register(registry: &mut super::feature_registry::FeatureRegistry)
+        where
+            Self: Sized,
+        {
+            registry.register::<DummyStage2, Self>(PipelineStageLifetime::PreProcess, Self);
+            registry.register::<DummyStage3, Self>(PipelineStageLifetime::PostProcess, Self);
+        }
+
+        fn run(
+            &mut self,
+            ctx: &mut BlogContext,
+            pipeline_type: std::any::TypeId,
+            lifetime: PipelineStageLifetime,
+        ) -> anyhow::Result<()> {
+            if pipeline_type == TypeId::of::<DummyStage2>()
+                && lifetime == PipelineStageLifetime::PreProcess
+            {
+                ctx.config.blog_author.push_str("[AAA]");
+                Ok(())
+            } else if pipeline_type == TypeId::of::<DummyStage3>()
+                && lifetime == PipelineStageLifetime::PostProcess
+            {
+                ctx.config.blog_author.push_str("[BBB]");
+                Ok(())
+            } else {
+                Ok(())
+            }
+        }
+    }
+
+    impl Feature for DummyFeature2 {
+        fn register(registry: &mut super::feature_registry::FeatureRegistry)
+        where
+            Self: Sized,
+        {
+            registry.register::<DummyStage2, Self>(PipelineStageLifetime::PreProcess, Self);
+            registry.register::<DummyStage3, Self>(PipelineStageLifetime::PostProcess, Self);
+        }
+
+        fn run(
+            &mut self,
+            ctx: &mut BlogContext,
+            pipeline_type: std::any::TypeId,
+            lifetime: PipelineStageLifetime,
+        ) -> anyhow::Result<()> {
+            if pipeline_type == TypeId::of::<DummyStage2>()
+                && lifetime == PipelineStageLifetime::PreProcess
+            {
+                ctx.config.blog_author.push_str("[CCC]");
+                Ok(())
+            } else if pipeline_type == TypeId::of::<DummyStage3>()
+                && lifetime == PipelineStageLifetime::PostProcess
+            {
+                ctx.config.blog_author.push_str("[DDD]");
+                Ok(())
+            } else {
+                Ok(())
+            }
+        }
+    }
 
     #[test]
-    fn pipeline_execution_order() {
-        struct DummyStage1;
-        struct DummyStage2;
-        struct DummyStage3;
-
-        impl PipelineStage for DummyStage1 {
-            fn initialize(&self, ctx: &mut BlogContext) -> anyhow::Result<()> {
-                ctx.config.blog_author.push_str("1i");
-                Ok(())
-            }
-
-            fn process(&self, ctx: &mut BlogContext) -> anyhow::Result<()> {
-                ctx.config.blog_author.push_str("1p");
-                Ok(())
-            }
-
-            fn finalize(&self, ctx: &mut BlogContext) -> anyhow::Result<()> {
-                ctx.config.blog_author.push_str("1f");
-                Ok(())
-            }
-        }
-
-        impl PipelineStage for DummyStage2 {
-            fn initialize(&self, ctx: &mut BlogContext) -> anyhow::Result<()> {
-                ctx.config.blog_author.push_str("2i");
-                Ok(())
-            }
-
-            fn process(&self, ctx: &mut BlogContext) -> anyhow::Result<()> {
-                ctx.config.blog_author.push_str("2p");
-                Ok(())
-            }
-
-            fn finalize(&self, ctx: &mut BlogContext) -> anyhow::Result<()> {
-                ctx.config.blog_author.push_str("2f");
-                Ok(())
-            }
-        }
-
-        impl PipelineStage for DummyStage3 {
-            fn initialize(&self, ctx: &mut BlogContext) -> anyhow::Result<()> {
-                ctx.config.blog_author.push_str("3i");
-                Ok(())
-            }
-
-            fn process(&self, ctx: &mut BlogContext) -> anyhow::Result<()> {
-                ctx.config.blog_author.push_str("3p");
-                Ok(())
-            }
-
-            fn finalize(&self, ctx: &mut BlogContext) -> anyhow::Result<()> {
-                ctx.config.blog_author.push_str("3f");
-                Ok(())
-            }
-        }
-
+    fn pipeline_stage_execution_order() {
         let ctx = BlogContext::default();
         let mut pipeline = Pipeline::new(ctx);
         pipeline.add_stage(DummyStage1);
@@ -133,5 +199,40 @@ mod test {
         let res = pipeline.run();
         assert!(res.is_ok());
         assert_eq!(pipeline.context.config.blog_author, "1i1p1f2i2p2f3i3p3f");
+    }
+
+    #[test]
+    fn pipeline_feature_stage_modification() {
+        let ctx = BlogContext::default();
+        let mut pipeline = Pipeline::new(ctx);
+        pipeline.add_stage(DummyStage1);
+        pipeline.add_stage(DummyStage2);
+        pipeline.add_stage(DummyStage3);
+        pipeline.add_feature::<DummyFeature1>();
+        let res = pipeline.run();
+        assert!(res.is_ok());
+        assert_eq!(
+            pipeline.context.config.blog_author,
+            "1i1p1f2i[AAA]2p2f3i3p[BBB]3f"
+        );
+    }
+
+    #[test]
+    fn pipeline_feature_execution_order_based_on_addition_order() {
+        let ctx = BlogContext::default();
+        let mut pipeline = Pipeline::new(ctx);
+        pipeline.add_stage(DummyStage1);
+        pipeline.add_stage(DummyStage2);
+        pipeline.add_stage(DummyStage3);
+        pipeline.add_feature::<DummyFeature2>();
+        pipeline.add_feature::<DummyFeature1>();
+        let res = pipeline.run();
+        // Note: Features that modify the same stage, are executed in the order
+        // they got added to the pipeline
+        assert!(res.is_ok());
+        assert_eq!(
+            pipeline.context.config.blog_author,
+            "1i1p1f2i[CCC][AAA]2p2f3i3p[DDD][BBB]3f"
+        );
     }
 }
